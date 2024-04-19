13e83a2d20b276e0f3f1bddb47ef8f59bb929e60
Моисеев Дмитрий <cmudrec@mail.ru>
2024-04-19 09:18:40 +0300

    Создание класса BookControl

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,7 +21,22 @@ namespace ConsoleApp1
                 name = n;
             }
 
-            public string ToString()
+            public string GetZhanr()
+            {
+                return zhanr;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public string GetAuthor()
+            {
+                return author;
+            }
+
+            public override string ToString()
             {
                 return "Автор: " + author + ", Жанр: " + zhanr + ", Название: " + name + "\n";
             }
@@ -30,10 +45,54 @@ namespace ConsoleApp1
 
         class BookControl
         {
-            int n = 0;
+            private int n = 0;
+            private Book[] books;
+
             public BookControl(int n)
             {
                 this.n = n;
+                books = new Book[n];
+            }
+
+            public void EnterParamsBooks()
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Введите автора книги");
+                    string a = Console.ReadLine();
+
+                    Console.Write("Введите жанр книги");
+                    string z = Console.ReadLine();
+
+                    Console.Write("Введите название книги");
+                    string n = Console.ReadLine();
+
+                    books[i] = new Book(a, z, n);
+                }
+            }
+
+            public void PrintBooks()
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Записанные книги");
+                    Console.Write(books[i].ToString());
+                }
+            }
+
+            public void SortBookByZhanr()
+            {
+                books = books.OrderBy(x => x.GetZhanr()).ToArray();
+            }
+
+            public void SortBookByAuthor()
+            {
+                books = books.OrderBy(x => x.GetAuthor()).ToArray();
+            }
+
+            public void SortBookByName()
+            {
+                books = books.OrderBy(x => x.GetName()).ToArray();
             }
         };
 
